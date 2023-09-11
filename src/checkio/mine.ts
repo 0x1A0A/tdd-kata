6b01cef8843dcb55a25e771260395aaeacc9a3b7
0x1A0A <mask.monkey1994@gmail.com>
2023-09-11 20:14:58 +0700

    test: lightbulb with start timer

--- src/checkio/mine.ts
+++ src/checkio/mine.ts
@@ -18,10 +18,30 @@ export function longPressed(target: string, input: string): boolean {
   const target_comp = compress(target);
   const input_comp = compress(input);
 
-  if (target_comp.length != input_comp.length ) return false;
+  if (target_comp.length != input_comp.length) return false;
 
-  const wrong = target_comp.some((v,index)=> v[1] > input_comp[index][1]);
-  const repeat = target_comp.some((v,index)=> v[1] != input_comp[index][1]);
+  const wrong = target_comp.some((v, index) => v[1] > input_comp[index][1]);
+  const repeat = target_comp.some((v, index) => v[1] != input_comp[index][1]);
 
   return wrong ? false : repeat;
+}
+
+export function sumLight(els: Date[], start?: Date): number {
+  let diff = 0,
+    i = 1;
+  if (start) {
+    while (els.length) {
+      if (+start >= +els[0] && +start <= +els[1]) {
+        els[0] = start;
+        break;
+      }
+      els.splice(0, 2);
+    }
+  }
+
+  while (i < els.length) {
+    diff += +els[i] - +els[i - 1];
+    i += 2;
+  }
+  return diff / 1000;
 }
--- tests/checkio/mine.spec.ts
+++ tests/checkio/mine.spec.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from "vitest";
-import { longPressed } from "../../src/checkio/mine";
+import { longPressed, sumLight } from "../../src/checkio/mine";
 
 describe("Long press", () => {
   test("detect long press", () => {
@@ -10,5 +10,22 @@ describe("Long press", () => {
     ).toBeTruthy();
     expect(longPressed("welcome boss!", "welcooome bos!!")).toBeFalsy();
     expect(longPressed("welcome boss!", "welcooome bos!! yay!")).toBeFalsy();
+  });
+});
+
+describe("Lightbulb watch", () => {
+  test("start the timer when we start watching", () => {
+    expect(
+      sumLight(
+        [new Date(2015, 0, 0, 0, 0, 0), new Date(2015, 0, 0, 0, 0, 0)],
+        new Date(2015, 0, 0, 0, 0, 5),
+      ),
+    ).toEqual(0);
+    expect(
+      sumLight(
+        [new Date(2015, 0, 0, 0, 0, 0), new Date(2015, 0, 0, 0, 0, 10)],
+        new Date(2015, 0, 0, 0, 0, 5),
+      ),
+    ).toEqual(5);
   });
 });
