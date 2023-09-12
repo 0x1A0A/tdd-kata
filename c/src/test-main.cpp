a4aad71b6483e43fe9ba55713735dfbd0cebe064
0x1A0A <mask.monkey1994@gmail.com>
2023-09-12 21:00:03 +0700

    test: using catch2 for reverse integer test

--- c/src/test-main.cpp
+++ c/src/test-main.cpp
@@ -4,6 +4,22 @@ int add(int a, int b) {
 	return a+b;
 }
 
-TEST_CASE("Add number", "[catch2 test]") {
+long reverse_i(long a) {
+	long result = 0;
+	while (a) {
+		result *=10;
+		result += a%10;
+		a /= 10;	
+	}
+	return result;
+}
+
+TEST_CASE("Add number", "[catch2 test add]") {
 	REQUIRE(add(1,2)==3);
+}
+
+TEST_CASE("Reverse number", "[catch2 test reverse]") {
+	REQUIRE(reverse_i(10) == 1);
+	REQUIRE(reverse_i(12) == 21);
+	REQUIRE(reverse_i(100009) == 900001);
 }
