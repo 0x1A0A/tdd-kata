c7c5861949a5db9330920e72441998c08ebbce78
0x1A0A <mask.monkey1994@gmail.com>
2023-09-13 22:16:06 +0700

    refactor: make an inline function

--- c/src/codewars/bit-counting.h
+++ c/src/codewars/bit-counting.h
@@ -1,3 +1,10 @@
 #include <stdint.h>
 
-uint16_t countBits(uint64_t n);
+inline uint16_t countBits(uint64_t n) {
+  uint16_t count = 0;
+  while (n) {
+    count += n & 1;
+    n >>= 1;
+  }
+  return count;
+}
--- c/src/codewars/string-end-with.h
+++ c/src/codewars/string-end-with.h
@@ -1,3 +1,6 @@
 #include <string>
 
-bool string_endwith(const std::string &str, const std::string &end);
+inline bool string_endwith(const std::string &str, const std::string &end) {
+  return str.length() >= end.length() &&
+         str.compare(str.length() - end.length(), end.length(), end) == 0;
+}
