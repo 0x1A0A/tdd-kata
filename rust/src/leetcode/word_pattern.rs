1351aedbf4903a66e82617143cd14fc95bcc862d
0x1A0A <mask.monkey1994@gmail.com>
2023-12-29 17:15:08 +0700

    test: rust leetcode 290

---
+++ rust/src/leetcode/word_pattern.rs
@@ -0,0 +1,59 @@
+struct Solution;
+
+impl Solution {
+    pub fn word_pattern(pattern: String, s: String) -> bool {
+        let mut map: std::collections::HashMap<char, &str> = std::collections::HashMap::new();
+        let mut set: std::collections::HashSet<&str> = std::collections::HashSet::new();
+
+        let word: Vec<&str> = s.split(" ").collect();
+
+        for (i, v) in pattern.chars().into_iter().enumerate() {
+            if let Some(w) = map.get(&v) {
+                if *w != word[i] {
+                    return false;
+                }
+                continue;
+            }
+
+            if let Some(w) = set.get(word[i]) {
+                return false;
+            }
+
+            map.insert(v, word[i]);
+            set.insert(word[i]);
+        }
+
+        println!("{:?}", map);
+
+        true
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn can_detect_one_pattern() {
+        let resutl = Solution::word_pattern("a".to_owned(), "ok".to_owned());
+        assert!(resutl)
+    }
+
+    #[test]
+    fn can_detect_two_same_pattern() {
+        let resutl = Solution::word_pattern("aa".to_owned(), "ok ok".to_owned());
+        assert!(resutl)
+    }
+
+    #[test]
+    fn can_detect_two_same_pattern_but_not_same_word() {
+        let resutl = Solution::word_pattern("aa".to_owned(), "ok no".to_owned());
+        assert!(resutl == false)
+    }
+
+    #[test]
+    fn can_detect_two_different_pattern() {
+        let resutl = Solution::word_pattern("ab".to_owned(), "ok ok".to_owned());
+        assert!(resutl == false)
+    }
+}
