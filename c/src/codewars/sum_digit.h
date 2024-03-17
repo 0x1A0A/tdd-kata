3c55b33e47eff40bd3a4feddc7b933f8fa7692a1
0x1A0A <mask.monkey1994@gmail.com>
2024-03-18 00:51:47 +0700

    refactor: digital sum using modulo instead of loop

--- c/src/codewars/sum_digit.h
+++ c/src/codewars/sum_digit.h
@@ -1,15 +1,1 @@
-inline int digit_root(int n) {
-  int sum = 0;
-
-  while (n) {
-    sum += n % 10;
-    n /= 10;
-
-    if (!n && sum / 10) {
-      n = sum;
-      sum = 0;
-    }
-  }
-
-  return sum;
-}
+inline int digit_root(int n) { return --n % 9 + 1; }
