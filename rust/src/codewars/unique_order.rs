f421fe65fa6ecdb24ebdc26e178be284c2706cd0
0x1A0A <mask.monkey1994@gmail.com>
2024-07-03 22:02:03 +0700

    refactor: codewars use dedup instead

--- rust/src/codewars/unique_order.rs
+++ rust/src/codewars/unique_order.rs
@@ -3,31 +3,8 @@ where
     T: std::iter::IntoIterator,
     T::Item: std::cmp::PartialEq + std::fmt::Debug,
 {
-    let mut peek = sequence.into_iter().peekable();
-    let mut result = vec![];
-
-    loop {
-        let x = peek.next();
-
-        match x {
-            None => break,
-            Some(x) => {
-                let n = peek.peek();
-                match n {
-                    None => {
-                        result.push(x);
-                        break;
-                    }
-                    Some(c) => {
-                        if c.ne(&x) {
-                            result.push(x);
-                        }
-                    }
-                }
-            }
-        }
-    }
-
+    let mut result = sequence.into_iter().collect::<Vec<_>>();
+    result.dedup();
     result
 }
 
