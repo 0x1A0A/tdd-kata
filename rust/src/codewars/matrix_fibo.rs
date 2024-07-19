6ec310185dca030a528440816851939a6cf54638
0x1A0A <mask.monkey1994@gmail.com>
2024-07-19 08:49:37 +0700

    test: codewars update fibo to support negative case

--- rust/src/codewars/matrix_fibo.rs
+++ rust/src/codewars/matrix_fibo.rs
@@ -1,11 +1,8 @@
 use num::bigint::BigInt;
 
 fn fib(n: i32) -> BigInt {
-    if n < 0 {
-        return BigInt::ZERO;
-    }
-    // this is positive case only
-    fib_iter(1.into(), 0.into(), 0.into(), 1.into(), n)
+    fib_iter(1.into(), 0.into(), 0.into(), 1.into(), n.abs())
+        * if n < 0 && n % 2 == 0 { -1 } else { 1 }
 }
 
 fn fib_iter(a: BigInt, b: BigInt, p: BigInt, q: BigInt, count: i32) -> BigInt {
@@ -19,7 +16,7 @@ fn fib_iter(a: BigInt, b: BigInt, p: BigInt, q: BigInt, count: i32) -> BigInt {
         let new_q = 2 * p.clone() * q.clone() + q.pow(2);
         fib_iter(a, b, pp + qq, new_q, count / 2)
     } else {
-        let new_a = a.clone() * (p.clone() + a.clone()) + b.clone() * q.clone();
+        let new_a = a.clone() * (p.clone() + q.clone()) + b.clone() * q.clone();
         let new_b = a.clone() * q.clone() + b.clone() * p.clone();
         fib_iter(new_a, new_b, p, q, count - 1)
     }
@@ -48,11 +45,11 @@ mod sample_tests {
         dotest(3, BigInt::from(2));
         dotest(4, BigInt::from(3));
         dotest(5, BigInt::from(5));
+        dotest(96, BigInt::from_str("51680708854858323072").unwrap());
     }
 
     // TODO: handle nagative case
     #[test]
-    #[ignore]
     fn small_negative_numbers() {
         dotest(-1, BigInt::from(1));
         dotest(-6, BigInt::from(-8));
@@ -60,7 +57,6 @@ mod sample_tests {
     }
 
     #[test]
-    #[ignore]
     fn large_numbers() {
         dotest(
             -500,
